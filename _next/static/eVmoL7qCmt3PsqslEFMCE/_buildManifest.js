24d4254d4e829308d4c564c3dc65722f2021ba73
SM Agency <167459621+sm-agency@users.noreply.github.com>
2025-07-20 02:46:07 +0200

    Add files via upload

---
+++ _next/static/eVmoL7qCmt3PsqslEFMCE/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-72c6142801778078.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
